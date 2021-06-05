8828aa1e8a652738da36fd8f8650e6fc302a0320
kumarjitc <kumarjit.chakraborty.1981@gmail.com>
2021-06-06 00:34:27 +0100

    Removed Unused Code

--- scripts/setup-gcloud.sh
+++ scripts/setup-gcloud.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export SDK_FILENAME=google-cloud-sdk-343.0.0-linux-x86_64.tar.gz
 curl -O -J curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-343.0.0-linux-x86_64.tar.gz
 tar -zxvf google-cloud-sdk-343.0.0-linux-x86_64.tar.gz --directory .
 ./google-cloud-sdk/install.sh
